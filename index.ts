8614fe3978f82bf814378138ed6fc6123843ac51
Safiyya <safiyya.babio@gmail.com>
2020-07-01 15:34:42 +0100

    csv export

--- index.ts
+++ index.ts
@@ -1,30 +1,52 @@
 import { predict } from "./predict";
 import { loadModel, loadMetadata } from "./loader";
-import { getAllMessages } from "./messages";
+import { getGroupChannelsMessages, Message } from "./messages";
+import { createObjectCsvWriter } from "csv-writer";
+import moment from "moment";
+import { compact } from "lodash";
+
+export interface Result {
+  date: string;
+  author: string;
+  sentence: string;
+  score: number;
+  excluded_words: string[];
+}
 
 const run = async () => {
   const model = await loadModel();
   const metadata = await loadMetadata();
 
-  const messages = await getAllMessages(CHANNEL_ID);
+  const messages = await getGroupChannelsMessages();
   if (!messages) return;
-  Promise.all(
-    messages.map((message) => predict(message.message, model, metadata))
-  ).then((results) => {
-    results.forEach((result, i) => {
-      console.log("***************");
-      console.log(result.sentence);
-      console.log(`score : ${result.score}`);
-      console.log(`sentiment : ${result.sentiment}`);
-      console.log(`excluded words : ${result.excluded}`);
-      console.log(`author:  ${messages[i].user.nickname}`);
-      console.log(
-        `createdAt: ${new Date(messages[i].created_at).toISOString()}`
-      );
-    });
-  });
-};
 
-const CHANNEL_ID = process.argv[2];
+  const writer = createObjectCsvWriter({
+    path: "./results.csv",
+    header: [
+      { id: "createdAt", title: "Created at" },
+      { id: "author", title: "Author" },
+      { id: "sentence", title: "Sentence" },
+      { id: "score", title: "Score" },
+      { id: "excluded_words", title: "Exculded words" },
+    ],
+  });
+
+  const results = await Promise.all(
+    compact(messages).map((message) => {
+      return predict(message.message, model, metadata);
+    })
+  );
+
+  console.log(results);
+
+  const records = results.map((result, i) => ({
+    createdAt: moment(messages[i].created_at).toISOString(),
+    author: messages[i].user.nickname,
+    sentence: result.sentence,
+    score: result.score,
+    excluded_words: result.excluded,
+  }));
+  await writer.writeRecords(records);
+};
 
 run();
